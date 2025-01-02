2b423d2ba31484ab220d6636959f91ba96a5bffe
Rayyhan <127735172+veryvelestial@users.noreply.github.com>
2025-01-02 13:21:26 +0800

    Update quiz.cc

--- 5-constants-and-strings/5.7-std::strings/quiz.cc
+++ 5-constants-and-strings/5.7-std::strings/quiz.cc
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
 
+int getAge()
+{
+	std::cout << "Enter your age: ";
+	int x{};
+	std::cin >> x;
+	return x;
+}
+
+int main()
+{
+	std::cout << "Enter your full name: ";
+	std::string name{};
+	std::getline(std::cin >> std::ws, name);
+
+	int age{ getAge() };
+
+	int nameLength{ static_cast<int>(std::ssize(name)) };
+
+	std::cout << "Your age + length of name is: " << nameLength + age << '\n';
+
+	return 0;
+}
